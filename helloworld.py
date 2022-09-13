0e207174e28fb8acac8fb91257b2e08c4e84bd93
ianmease8 <imease8@gmail.com>
2022-09-13 14:34:02 -0600

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,42 @@
+#here is my hello world file 
+
+from ast import main
+
+
+class __main__():
+    if __name__ == "__main__":
+        main
+
+class h():
+    h
+
+class e():
+    e
+
+class l():
+    l
+
+class l1():
+    l1
+
+class h():
+    h
+
+class h():
+    h
+
+class h():
+    h
+
+class h():
+    h
+
+class h():
+    h
+
+class h():
+    h
+
+class h():
+    h    
+print("hello world")
